ca0dd498ebe17a7fcc97f1ec0611ddf1ed76d2f4
zengingrid@gmail.com <zengingrid@gmail.com>
2022-12-07 16:01:46 -0800

    terraform

--- dev/output.tf
+++ dev/output.tf
@@ -1,10 +1,10 @@
 output "frontend_ip" {
-  value = digitalocean_droplet.frontend.ipv4_address
+  value = digitalocean_droplet.frontend_app.ipv4_address
   description = "The ip address of the frontend"
 }
 
 
 output "application_ip" {
-  value = digitalocean_droplet.application.ipv4_address
+  value = digitalocean_droplet.application_app.ipv4_address
   description = "The ip address of the application"
 }
